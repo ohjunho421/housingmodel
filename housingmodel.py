5dbd712700d5922ef6d717cac9f9f75cdf46cca5
Rorarion <wnsghcoswp@gmail.com>
2024-12-18 20:41:11 +0900

    create hosingmodel.py

---
+++ housingmodel.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+
+# Load model
+model = joblib.load("gradient_boosting_model.pkl")
+
+# Streamlit app
+st.title("주택가격예측모델")
+st.write("가격예측을위한 지표값을 넣어주세요.")
+
+# Input fields
+LSTAT = st.number_input("LSTAT (% 인구밀도)")
+RM = st.number_input("RM (주거지당 평균 방 수)")
+PTRATIO = st.number_input("도시별 학생-교사 비율)")
+
+# Predict button
+if st.button("Predict"):
+    X_new = [[LSTAT, RM, PTRATIO]]
+    prediction = model.predict(X_new)[0]
+    st.write(f"Predicted value: ${prediction * 1000:.2f}")
